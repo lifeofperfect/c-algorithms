034fdc85e27ec8c313a6773a8713e1a1dafc8f0b
lifeofperfect <perfect.igbadumhe@gmail.com>
2020-08-27 10:52:13 +0100

    factorial logic

---
+++ Remove Palindromic sequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Remove_Palindromic_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "loml";
+
+            char[] arr = text.ToCharArray();
+
+            Array.Reverse(arr);
+
+            string rev = new string(arr);
+
+            if (text == "")
+            {
+                Console.WriteLine(0);
+            }
+
+            if(text.Equals(rev))
+                Console.WriteLine(1);
+
+            else
+                Console.WriteLine(2);
+            
+
+           
+        }
+    }
+}
---
+++ factorialProblem/Factorial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask for a number
+            Console.WriteLine("Please provide a number");
+            int factorialNumber = Int32.Parse(Console.ReadLine());
+
+            //result of the factorial
+            int factorialResult = 1;
+
+            //check if provided number is less or greater than zero
+            if(factorialNumber < 0)
+            {
+                Console.WriteLine("Please provide a number greater than 0");
+            }else
+            {
+                //find factorial
+                for (int i = 1; i <= factorialNumber; i++)
+                {
+                    factorialResult = factorialResult * i;
+                }
+
+                //result of the factorial
+                Console.WriteLine("{0}! has a factorial of {1}", factorialNumber, factorialResult);
+            }
+
+            
+        }
+    }
+}
